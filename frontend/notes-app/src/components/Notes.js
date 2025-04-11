08ba550c833e6694245aea927fd07ac159ae063d
Hari Sirikonda <harisirikonda22@gmail.com>
2025-04-11 20:09:20 +0530

    Added pin button to note card

--- frontend/notes-app/src/components/Notes.js
+++ frontend/notes-app/src/components/Notes.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Edit from '../assets/edit.png';
 import Delete from '../assets/delete.png';
+import Pin from '../assets/pin.png';
+import Pinned from '../assets/pinned.png';
 
 function Notes({ title, date, discription, status }) {
+    const [pinned, setPinned] = useState(true);
     return (
         <div>
             <div className='box border-0 rounded shadow p-3 m-1 mb-3' style={{ width: '420px' }}>
@@ -11,6 +14,7 @@ function Notes({ title, date, discription, status }) {
                         <h4 className='m-0'>{title}</h4>
                     </div>
                     <div className='d-flex align-items-center ms-auto'>
+
                         <button className='btn shadow-sm'>
                             <img src={Edit} alt='edit' style={{ width: '20px', height: '20px' }} />
                         </button>
@@ -31,13 +35,24 @@ function Notes({ title, date, discription, status }) {
                         </div>
                     </div>
                 </div>
-                <div className='ps-1 text-dark'><span className='bg-light rounded px-1'>{date}</span></div>
+                <div className='ps-1 text-dark align-items-center justify-content-center'>
+                    <span className='bg-light rounded px-1'>{date}</span>
+                    <span></span>
+                </div>
                 <div className='p-1'>
                     {discription}
                 </div>
-                <div className='p-1'>
-                    --- Attachments
+                <div className='d-flex p-1'>
+                    <div>
+                        --- Attachments
+                    </div>
                 </div>
+                <div className="d-flex justify-content-end">
+                    <button className='btn shadow-sm' onClick={() => { setPinned(!pinned) }}>
+                        <img src={pinned ? Pinned : Pin} alt='show me' style={{ width: "20px", height: "20px" }} />
+                    </button>
+                </div>
+
             </div>
         </div>
     )
