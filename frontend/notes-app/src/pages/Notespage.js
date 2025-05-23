ba5a03fa2435da553b81836cfd7d024be6e728b8
Hari Sirikonda <harisirikonda22@gmail.com>
2025-05-23 11:54:18 +0530

    Update finalized

--- frontend/notes-app/src/pages/Notespage.js
+++ frontend/notes-app/src/pages/Notespage.js
@@ -4,7 +4,6 @@ import Notes from '../components/Notes';
 import PlusIcon from '../assets/plus.png';
 import CloseIcon from '../assets/remove.png';
 import axiosInstance from '../utils/axiosInstance';
-import Toast from '../components/Toast';
 
 
 function Notespage() {
@@ -127,7 +126,6 @@ function Notespage() {
     return (
         <div>
             <Navbar showSearch={true} showLR={!isLoggedIn} showProfile={true} />
-            <Toast />
             {showConfirm && (
                 <div className="alert alert-danger border-0 shadow-sm text-dark slide-down confirm-delete d-flex align-items-center jusrify-content-center" role="alert" style={{ width: "450px", height: "60px" }}>
                     <span className='mx-3'>Are you sure, want to delete ?</span>
