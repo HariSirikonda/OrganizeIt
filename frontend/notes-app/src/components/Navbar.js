c8d19e6d1c01a32c0915b0c56cefe3cc8ea8ef21
Hari Sirikonda <harisirikonda22@gmail.com>
2025-06-06 12:23:01 +0530

    Fixed the navbar to top

--- frontend/notes-app/src/components/Navbar.js
+++ frontend/notes-app/src/components/Navbar.js
@@ -65,7 +65,7 @@ function Navbar() {
 
     return (
         <div>
-            <nav class="navbar nav-color navbar-expand-lg">
+            <nav class="navbar nav-color navbar-expand-lg fixed-top">
                 <div class="container-fluid">
                     <Link class="navbar-brand text-white" to='/'><b>OrganizeIt</b></Link>
                     <div class="collapse navbar-collapse">
